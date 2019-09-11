51d88dd8e5ff2cbe68aa6a51ec3a4fa5bc33025c
git2hub17 <swpuujee@gmail.com>
2019-09-10 20:02:03 -0500

    This is my changes. Lab 7

--- lab07_2/Main_lab07_2.js
+++ lab07_2/Main_lab07_2.js
@@ -1,46 +1,35 @@
 
-//ex4. isVovel()
-function isVowel(char) {
+//ex4. sum()
+    const array1 = [1,2,3,4];
+    const sum = (acc, currVal) => acc + currVal;
+    console.log(array1.reduce(sum));
 
+    const multiplier = (acc, val) => acc * val;
+    console.log(array1.reduce(multiplier));
+
+    let numbers = [1,2,3,4];
+    let multipliedNumbers = numbers.map(n => n * n);
+    let addedNumbers = numbers.map(n => n + n);
+    console.log(multipliedNumbers);
+    console.log(addedNumbers);
+    console.log(numbers);
+
+
+    //ex5.
+function reverse(str){
+    return str.split("").reduce((rev, char)=> char + rev, '');
 }
-//ex5. sum()
-// function sumArr(mult, base, numbers) {
-//     const total = [0,1,2,3].reduce((accum, num) => accum + num, base);
-//     return mult * total;
-//
-// }
-//
-// let numbers = [1,2,3,4];
-// let multipliedNumbers = numbers.map(n => n * n);
-// let addedNumbers = numbers.map(n => n + n);
-// console.log(multipliedNumbers);
-// console.log(addedNumbers);
-// console.log(numbers);
 
-// function sum(multiplier, base, numbers) {
-//     var temp = numbers.reduce((accum, num) => accum + num, base);
-//     return multiplier * temp;
-// }
-//
-// var total = sum(2, 6, 10, 9, 8);
-// console.log("total: " + total);
-//
-
-// function calc(multiplier, base, ...numbers) {
-//     var temp = numbers.reduce((accum, num) => accum + num, base);
-//     return multiplier * temp;
-// }
-// var total = calc(2, 6, 10, 9, 8); console.log("total: " + total);
-
-
-// ex7. findLongestWord
-function findLongestWord(arr) {
-    let maxstr = arr[0].length, i
-    let len = arr.length
-    for (i = 0; i < len; i++) {
-        if (arr[i].length > maxstr)
-            maxstr = arr[i]
-    }
-    return maxstr
+    // ex7. filterLongWords()
+function findLongestWord(longestWord) {
+    const word = ["USA", "Mongolia", "Canada", "Korea"];
+    return maxStr = longestWord.filter(word => word.length > 6);
 }
-document.write("7. Longest Word of array (USA, Mongolia, Canada, Korea): " + findLongestWord(["USA", "Mongolia", "Canada", "Korea"]) + "<br>")
+
+const longestWord = ["USA", "Mongolia", "Canada", "Korea"];
+    const result = longestWord.filter(word => word.length > 6);
+    console.log(result)
+
+
+
+
